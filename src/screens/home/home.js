3cb706ce8f83743e7d6e4aa9441be32b79d70a3c
Roshan Nizar <IT17048774@my.sliit.lk>
2020-04-29 18:04:00 +0530

    Create home.js

---
+++ src/screens/home/home.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { BottomNavigation, Text } from 'react-native-paper';
+
+class Home extends Component {
+
+    state = {
+        index: 0,
+        routes: [
+            { key: 'home', title: 'Home', icon: 'home' },
+            { key: 'search', title: 'Search', icon: 'magnify' },
+            { key: 'cart', title: 'Cart', icon: 'cart' },
+            { key: 'profile', title: 'Profile', icon: 'account' }
+        ],
+    };
+
+    _handleIndexChange = index => this.setState({ index });
+
+    _renderScene = BottomNavigation.SceneMap({
+        home: MusicRoute,
+        search: AlbumsRoute,
+        cart: RecentsRoute,
+        profile: RecentsRoute
+    });
+
+    render() {
+        return (
+            <BottomNavigation
+                barStyle={{ backgroundColor: '#00aced' }}
+                navigationState={this.state}
+                onIndexChange={this._handleIndexChange}
+                renderScene={this._renderScene}
+            />
+        );
+    }
+}
+
+const MusicRoute = () => <Text>Music</Text>;
+
+const AlbumsRoute = () => <Text>Albums</Text>;
+
+const RecentsRoute = () => <Text>Recents</Text>;
+
+export default Home;
