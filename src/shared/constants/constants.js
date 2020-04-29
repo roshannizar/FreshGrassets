74a3e878df3c2a5d9b693782b1331916bbe449a3
Roshan Nizar <IT17048774@my.sliit.lk>
2020-04-29 18:04:10 +0530

    Create constants.js

---
+++ src/shared/constants/constants.js
@@ -0,0 +1,4 @@
+export const GET_ERRORS = 'GET_ERRORS';
+export const SET_CURRENT_USER = 'SET_CURRENT_USER';
+export const PROGRAM_LOADING = 'PROGRAM_LOADING';
+export const INVALID_USER = 'INVALID_USER';
