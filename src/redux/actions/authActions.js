4a75d43ea060acb59f71db386bd3ef24af833992
Roshan Nizar <IT17048774@my.sliit.lk>
2020-04-30 16:39:55 +0530

    Update authActions.js

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -3,21 +3,33 @@ import { AsyncStorage } from 'react-native';
 import { INVALID_USER, SET_CURRENT_USER } from '../../shared/constants/constants';
 import keys from '../../utility/keys/keys';
 
-export const loginUser = (userData) => dispatch => {
+export const loginUser = (data) => dispatch => {
+
     const headers = {
-        'Content-Type':'application/x-www-form-urlencoded'
-    };
+        'Content-Type': 'application/json'
+    }
 
     axios
-    .post(`${keys.auth}/v1/signin`,headers)
-    .then(res => {
-        console.log(res);
-    })
-    .catch(err => dispatch({
-        type: INVALID_USER,
-        payload:'400 Not Found'
-    }));
+        .post(`${keys.auth}/v1/signin`, data, headers)
+        .then(res => {
+            AsyncStorage.clear();
+            AsyncStorage.setItem('user', res.data);
+            dispatch(setCurrentUser(res.data))
+            console.log(res.data);
+        })
+        .catch(err => {
+            console.log(err);
+            dispatch({
+                type: INVALID_USER,
+                payload: '400 Not Found'
+            })
+        });
 }
+
+export const logoutUser = () => dispatch => {
+    AsyncStorage.removeItem('user');
+    dispatch(setCurrentUser({}));
+};
 
 export const setCurrentUser = decoded => {
     return {
