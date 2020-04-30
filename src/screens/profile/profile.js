2479b6d7c11cd3eefc0e9e94aec5debd1b96a8fe
Roshan Nizar <IT17048774@my.sliit.lk>
2020-04-30 17:54:38 +0530

    Update profile.js

--- src/screens/profile/profile.js
+++ src/screens/profile/profile.js
@@ -1,20 +1,45 @@
-import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Appbar } from 'react-native-paper';
+import { logoutUser } from '../../redux/actions/authActions';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Actions } from 'react-native-router-flux';
 
 class Profile extends Component {
-    render(){
-        return(
-            <View style={styles.container}>
-                <Text>Santa this is profile</Text>
+
+    constructor(props) {
+        super(props);
+        this._handleLogout = this._handleLogout.bind(this);
+    }
+
+    _handleLogout() {
+        this.props.logoutUser();
+        Actions.authentication();
+    }
+
+    render() {
+        return (
+            <View>
+                <Appbar.Header style={{ backgroundColor: '#00aced' }}>
+                    <Appbar.Content title='Profile' />
+                    <Appbar.Action icon='logout' onPress={this._handleLogout} />
+                </Appbar.Header>
             </View>
         );
     }
 }
 
-export default Profile;
+Profile.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+    auth: state.auth
+})
+
+export default connect(mapStateToProps, { logoutUser })(Profile);
 
 const styles = StyleSheet.create({
-    container: {
-        marginTop:20
-    }
 });
