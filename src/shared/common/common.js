cae7a25047862570a81cf973be44fdb100985882
Roshan Nizar <IT17048774@my.sliit.lk>
2020-05-06 18:13:38 +0530

    Create common.js

---
+++ src/shared/common/common.js
@@ -0,0 +1,21 @@
+export const common = {
+    chipData: [{
+        id: '1',
+        name: 'All Category',
+        selected: true
+    }, {
+        id: '2',
+        name: 'Vegetables',
+        selected: false
+    }, {
+        id: '3',
+        name: 'Fruits',
+        selected: false
+    }, {
+        id: '4',
+        name: 'Others',
+        selected: false
+    }]
+};
+
+module.exports = common;
