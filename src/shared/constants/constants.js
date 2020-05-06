48790099b601d53fada55a288cb9b6a33aaef6ab
Roshan Nizar <IT17048774@my.sliit.lk>
2020-05-06 18:13:40 +0530

    Update constants.js

--- src/shared/constants/constants.js
+++ src/shared/constants/constants.js
@@ -2,3 +2,4 @@ export const GET_ERRORS = 'GET_ERRORS';
 export const SET_CURRENT_USER = 'SET_CURRENT_USER';
 export const PROGRAM_LOADING = 'PROGRAM_LOADING';
 export const INVALID_USER = 'INVALID_USER';
+export const GET_PRODUCTS = 'GET_PRODUCTS';
