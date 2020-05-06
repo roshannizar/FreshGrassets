52424261025ea833344a7be3c8b84f818ef4a137
Roshan Nizar <IT17048774@my.sliit.lk>
2020-05-06 18:13:21 +0530

    Update index.js

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import AuthReducer from './authReducer';
+import ProductsReducer from './productsReducer';
 
 export default combineReducers({
-    auth: AuthReducer
+    auth: AuthReducer,
+    product: ProductsReducer
 });
