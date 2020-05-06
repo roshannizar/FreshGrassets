c864eb2cfc825219e2e163edf5b7a5a9ea203812
Roshan Nizar <IT17048774@my.sliit.lk>
2020-05-06 23:20:39 +0530

    Update signin.js

--- src/screens/security/signin.js
+++ src/screens/security/signin.js
@@ -16,18 +16,20 @@ class SignIn extends Component {
             phone_number: '',
             password: '',
             boolNext: false,
-            signInText: 'Next'
+            signInText: 'Next',
+            error: false
         }
         this._handleLogin = this._handleLogin.bind(this);
     }
 
     static getDerivedStateFromProps(nextProps, state) {
-        if(nextProps.auth.isAuthenticated) {
+        if (nextProps.auth.isAuthenticated) {
             Actions.home();
         }
 
-        if(nextProps.auth.user === '400 Not Found') {
-            state.boolNext=false;
+        if (nextProps.auth.user === '400 Not Found') {
+            state.boolNext = false;
+            state.error = true;
         } else {
             return null;
         }
@@ -36,14 +38,14 @@ class SignIn extends Component {
     _handleLogin() {
         const { phone_number, password } = this.state;
         const data = {
-            phone_number: "+"+phone_number,
+            phone_number: "+" + phone_number,
             password: password
         }
         this.props.loginUser(data);
     }
 
     render() {
-        const { boolNext, signInText, phone_number, password } = this.state;
+        const { boolNext, signInText, phone_number, password, error } = this.state;
 
         return (
             <KeyboardAwareScrollView style={styles.container}>
@@ -70,10 +72,7 @@ class SignIn extends Component {
                             <Button
                                 mode="contained"
                                 color='#00aced'
-                                onPress={
-                                    // Actions.replace('home')
-                                    this._handleLogin
-                                }>
+                                onPress={this._handleLogin}>
                                 {signInText}
                             </Button>
                         </View>
@@ -87,6 +86,7 @@ class SignIn extends Component {
                                 underlineColor='#00aced'
                                 placeholderTextColor='#00aced'
                                 placeholder='eg: 94123467890'
+                                error={error}
                                 onChangeText={phone_number => this.setState({ phone_number })}
                             />
                             <View style={{ margin: 5 }}></View>
